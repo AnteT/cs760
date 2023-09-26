2bb3b37a26424bd6bc4a07f335243dac0370ee5e
Tonkovic-Capin <212765834@ge.com>
2023-09-26 16:28:53 -0500

    added generic plot bounds helper and revised lagrange modeling

--- HW2/lagrange_polynomial.py
+++ HW2/lagrange_polynomial.py
@@ -67,18 +67,34 @@ def get_uniform_distribution(a,b,size=100):
     samples = np.random.uniform(a,b,size)
     return samples
 
-def plot_sample(df:pd.DataFrame):
+def plot_sample(df:pd.DataFrame, title=None):
     x_val = df['x']
     y_val = df['y']
     plt.scatter(x_val,y_val)
-    plt.xlim(left=0,right=10)
+    # plt.xlim(left=0,right=10)
+    plt.title(title)
     plt.show()
 
-def generate_training_sample_dataframe(interval:list,size:int) -> pd.DataFrame:
+def generate_training_sample_dataframe(interval:list,size:int, add_gaussian_noise=True, mean=0, std=1.0, variant='pre') -> pd.DataFrame:
+    """variant types: `pre` adds noise to x before generating y, `post` adds noise to x after generating y"""
     start,stop = interval
-    n_samples = np.random.uniform(start,stop,size)
-    df = pd.DataFrame(data=n_samples,columns=['x'])
-    df['y'] = np.sin(df['x'])
+    x_sample = np.random.uniform(start,stop,size)
+    if not add_gaussian_noise:
+        df = pd.DataFrame(data=x_sample,columns=['x'])
+        df['y'] = np.sin(df['x'])
+        return df
+    
+    noise = np.random.normal(mean, std, len(x_sample))
+    if variant == 'pre':
+        x_sample = x_sample + noise
+        df = pd.DataFrame(data=x_sample,columns=['x'])
+        df['y'] = np.sin(df['x'])
+    elif variant == 'post':
+        df = pd.DataFrame(data=x_sample,columns=['x'])
+        df['y'] = np.sin(df['x'])    
+        data = df['x'].to_numpy(dtype=np.float64)
+        noise = np.random.normal(mean, std, data.shape)
+        df['x'] = pd.Series(data + noise)
     return df
 
 def get_lagrange_data(filename='lg_train.csv'):
@@ -116,29 +132,73 @@ def add_gaussian_noise_to_dataframe(df:pd.DataFrame, mean=0, std=1, target_colum
     df[target_column] = pd.Series(data + noise)
     return df
 
-######################## run main function ########################
-retrain = True
-plot = True
-include_MSE = False
-TRAINING_INTERVAL = [0,10]
-TRAINING_SET_SIZE = 100
-GAUSSIAN_NOISE_STD = 1
 
-if retrain:
-    df_training = generate_training_sample_dataframe(TRAINING_INTERVAL,TRAINING_SET_SIZE)
-    df_training = add_gaussian_noise_to_dataframe(df_training, mean=0, std=GAUSSIAN_NOISE_STD,target_column='y')
-    lg_polynomial = generate_langrange_model_from_dataframe(df_training) # returns a formula you can call like f(n)
-    training_error_dict = find_and_return_MSE_and_MAE_loss(lg_polynomial,df_training) # returns validation set across the same distribution as model
-    df_validation = generate_training_sample_dataframe(TRAINING_INTERVAL,100)
-    test_error_dict = find_and_return_MSE_and_MAE_loss(lg_polynomial, df_validation)
-    if not include_MSE:
+def train_and_validate_with_optional_noise_vary_by_training_size(TRAINING_SET_SIZE_BATCHES = (5,10,15,20,25,50,75,100), ADD_GAUSSIAN_NOISE=False):
+    TRAINING_INTERVAL = [0,10]
+    RESULTS_HEADERS = ['T_size', 'train_error', 'test_error','formula']
+    RESULTS_DATA = []
+    SHOW_FORMULA = False
+    SHOW_PLOT = False
+
+    TRAINING_SET_SIZE_BATCHES = (5,10,15,20,25,50,75,100) # partial
+    df_validation = generate_training_sample_dataframe(TRAINING_INTERVAL,100,add_gaussian_noise=False)
+
+    for training_size in TRAINING_SET_SIZE_BATCHES:
+        TRAINING_SET_SIZE = training_size
+        df_training = generate_training_sample_dataframe(TRAINING_INTERVAL,TRAINING_SET_SIZE,add_gaussian_noise=ADD_GAUSSIAN_NOISE,std=25,variant='pre') # pre is the right approach
+        if SHOW_PLOT:
+            plot_sample(df_training, title=f"Data for training size: {TRAINING_SET_SIZE}")
+        lg_polynomial = generate_langrange_model_from_dataframe(df_training) # returns a formula you can call like f(n)
+        training_error_dict = find_and_return_MSE_and_MAE_loss(lg_polynomial,df_training) # returns validation set across the same distribution as model
+        test_error_dict = find_and_return_MSE_and_MAE_loss(lg_polynomial, df_validation)
         training_error_dict.pop('MSE')
         test_error_dict.pop('MSE')
-    train_error, test_error = training_error_dict['MAE'], test_error_dict['MAE']
-    print(f'trained on {TRAINING_SET_SIZE}\ngaussian noise std: {GAUSSIAN_NOISE_STD}\ntraining error: {train_error}\ntest error: {test_error}')
+        lg_formula = lg_polynomial if SHOW_FORMULA else None
+        dyn_lg_formula = f'{lg_formula}' if SHOW_FORMULA else '' + '\n' if SHOW_FORMULA else ''
+        size_var, train_error, test_error = TRAINING_SET_SIZE, training_error_dict['MAE'], test_error_dict['MAE']
+        RESULTS_DATA.append([size_var, train_error, test_error, lg_formula])
+        print(f"""trained on {TRAINING_SET_SIZE}\ngaussian noise added: {ADD_GAUSSIAN_NOISE}\ntraining error: {train_error}\ntest error: {test_error}{dyn_lg_formula}""")
 
-### plot lagrange poly ###
-if plot:
-    x, y = df_training['x'], df_training['y']
-    plot_lagrange_polynomial(x,y) # first 5 only
-    # plot_lagrange_polynomial_and_all_basis(x,y) # all basis polynomials
+    print(f'\ntraining by varying sample size validated against 100 test samples, results across size:\n')
+    df_summary = pd.DataFrame(data=RESULTS_DATA,columns=RESULTS_HEADERS)
+    print(df_summary)
+
+def train_and_validate_with_gaussian_noise_vary_by_std(GAUSS_NOISE_STD_BATCHES:tuple = (0.1,0.5,1.0,1.5,2.0,3.0,10.0,100.0)):
+    TRAINING_INTERVAL = [0,10]
+    TRAINING_SET_SIZE = 100
+    RESULTS_HEADERS = ['std', 'train_error', 'test_error','formula']
+    RESULTS_DATA = []
+    SHOW_FORMULA = False
+    SHOW_PLOT = False
+
+    # GAUSS_NOISE_STD_BATCHES = (0.1,0.2,0.3,0.4,0.5,1.0,1.25,1.50,1.75,2.0,2.25,2.50,2.75,3.0,3.5,4.0,5.0,10.0,25.0,50.0,100.0) # full
+    GAUSS_NOISE_STD_BATCHES = (0.1,0.5,1.0,1.5,2.0,3.0,10.0,100.0) # partial
+    TRAINING_SET_SIZE_BATCHES = (5,10,15,20,25,50,75,100) # partial
+
+    df_validation = generate_training_sample_dataframe(TRAINING_INTERVAL,100,add_gaussian_noise=False)
+
+    for std in GAUSS_NOISE_STD_BATCHES:
+        df_training = generate_training_sample_dataframe(TRAINING_INTERVAL,TRAINING_SET_SIZE,add_gaussian_noise=True,std=std,variant='pre') # pre is the right approach
+        if SHOW_PLOT:
+            plot_sample(df_training, title=f"Data for STD: {std}")
+        lg_polynomial = generate_langrange_model_from_dataframe(df_training) # returns a formula you can call like f(n)
+        training_error_dict = find_and_return_MSE_and_MAE_loss(lg_polynomial,df_training) # returns validation set across the same distribution as model
+        test_error_dict = find_and_return_MSE_and_MAE_loss(lg_polynomial, df_validation)
+        training_error_dict.pop('MSE')
+        test_error_dict.pop('MSE')
+        lg_formula = lg_polynomial if SHOW_FORMULA else None
+        dyn_lg_formula = f'{lg_formula}' if SHOW_FORMULA else '' + '\n' if SHOW_FORMULA else ''
+        std_var, train_error, test_error = std, training_error_dict['MAE'], test_error_dict['MAE']
+        RESULTS_DATA.append([std_var, train_error, test_error, lg_formula])
+        print(f"""trained on {TRAINING_SET_SIZE}\ngaussian noise std: {std}\ntraining error: {train_error}\ntest error: {test_error}{dyn_lg_formula}""")
+
+    print(f'\ntraining with {TRAINING_SET_SIZE} samples validated against 100 test samples, results across std:\n')
+    df_summary = pd.DataFrame(data=RESULTS_DATA,columns=RESULTS_HEADERS)
+    print(df_summary)
+
+######################## run main function ########################
+### train and test varying gaussian noise ###
+train_and_validate_with_gaussian_noise_vary_by_std(GAUSS_NOISE_STD_BATCHES = (0.1,0.5,1.0,1.5,2.0,3.0,10.0,100.0))
+
+### train and test varying test size ###
+train_and_validate_with_optional_noise_vary_by_training_size(TRAINING_SET_SIZE_BATCHES = (5,10,15,20,25,30,40,50,75,100), ADD_GAUSSIAN_NOISE=True)
