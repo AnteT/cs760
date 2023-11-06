0327ccc29b79f73b9dd90a47b2cf2c4adc287904
ante <antetc@cs.wisc.edu>
2023-11-06 17:17:40 -0600

    added kmeans implementation

--- HW5/run_functions.py
+++ HW5/run_functions.py
@@ -1,3 +1,4 @@
+from dep_functions import get_x_train_y_train_stacked_samples, KMeansClustering
 from dep_functions import run_kmeans_and_gmm_experiment_for_k_clusters
 from dep_functions import plot_kmc_gmm_results, plot_generic_data
 from dep_functions import import_dataset
@@ -6,6 +7,14 @@ from dep_functions import dim100_loss_data
 
 ######################################## run main experiment ########################################
 if __name__ == '__main__':
+    centers = 3
+    for sigma in (0.5, 1, 2, 4, 8):
+        X_train, true_labels, label_dict = get_x_train_y_train_stacked_samples(sigma=sigma)
+        kmc = KMeansClustering(n_clusters=centers)
+        kmc.fit(X_train)
+        class_centers, classification = kmc.return_centroids(X_train)
+        kmc.return_current_accuracy()
+
     experiment_data = run_kmeans_and_gmm_experiment_for_k_clusters(k_clusters=3) # returns results data of experiment for both kmc and gmm
     plot_kmc_gmm_results(experiment_data, plot_type='accuracy', savefile='./data/figs/kmc-gmm-accuracy.png') # plots 2 side-by-side plots of objective for kmc vs gmm
     plot_kmc_gmm_results(experiment_data, plot_type='objective', savefile='./data/figs/kmc-gmm-objective.png') # plots 2 side-by-side plots of accuracy for kmc vs gmm
@@ -24,3 +33,4 @@ if __name__ == '__main__':
     master_PCA_by_type(input_matrix_1000d, n_dim=30, pca_type='demean', plot=False, savefile=None) # demeaned pca on chosen d=30
     master_PCA_by_type(input_matrix_1000d, n_dim=30, pca_type='norm', plot=False, savefile=None) # normalized pca on chosen d=30
     master_PCA_by_type(input_matrix_1000d, n_dim=30, pca_type='dro', plot=False, savefile=None) # dro on chosen d=30
+
