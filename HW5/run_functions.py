231812036f8a4c8d3b57ebe793c5a74cc5e942a5
ante <antetc@cs.wisc.edu>
2023-11-06 13:07:57 -0600

    initial for HW5 without dependencies

---
+++ HW5/run_functions.py
@@ -0,0 +1,26 @@
+from dep_functions import run_kmeans_and_gmm_experiment_for_k_clusters
+from dep_functions import plot_kmc_gmm_results, plot_generic_data
+from dep_functions import import_dataset
+from dep_functions import master_PCA_by_type
+from dep_functions import dim100_loss_data
+
+######################################## run main experiment ########################################
+if __name__ == '__main__':
+    experiment_data = run_kmeans_and_gmm_experiment_for_k_clusters(k_clusters=3) # returns results data of experiment for both kmc and gmm
+    plot_kmc_gmm_results(experiment_data, plot_type='accuracy', savefile='./data/figs/kmc-gmm-accuracy.png') # plots 2 side-by-side plots of objective for kmc vs gmm
+    plot_kmc_gmm_results(experiment_data, plot_type='objective', savefile='./data/figs/kmc-gmm-objective.png') # plots 2 side-by-side plots of accuracy for kmc vs gmm
+
+    plot_generic_data(dim100_loss_data, savefile='./data/figs/mse-loss-d30.png') # supporting figures for d=30
+    plot_generic_data(dim100_loss_data, first_order_diff=True, savefile='./data/figs/mse-loss-fod-d30.png') # supporting figures for d=30
+
+    input_matrix_2d = import_dataset(dataset='2d', to_numpy=True)
+    master_PCA_by_type(input_matrix_2d, n_dim=1, pca_type='buggy', plot=False, plot_title='Buggy PCA', savefile='./data/figs/buggy-pca.png') # run and plot buggy PCA
+    master_PCA_by_type(input_matrix_2d, n_dim=1, pca_type='demean', plot=False, plot_title='Demeaned PCA', savefile='./data/figs/demeaned-pca.png') # run and plot demeaned PCA
+    master_PCA_by_type(input_matrix_2d, n_dim=1, pca_type='norm', plot=False, plot_title='Normalized PCA', savefile='./data/figs/normalized-pca.png') # run and plot normalized PCA
+    master_PCA_by_type(input_matrix_2d, n_dim=1, pca_type='dro', plot=False, plot_title='DRO Analysis', savefile='./data/figs/dro-pca.png') # run and plot DRO analysis
+
+    input_matrix_1000d = import_dataset(dataset='1000d', to_numpy=True)
+    master_PCA_by_type(input_matrix_1000d, n_dim=30, pca_type='buggy', plot=False, savefile=None) # buggy pca on chosen d=30
+    master_PCA_by_type(input_matrix_1000d, n_dim=30, pca_type='demean', plot=False, savefile=None) # demeaned pca on chosen d=30
+    master_PCA_by_type(input_matrix_1000d, n_dim=30, pca_type='norm', plot=False, savefile=None) # normalized pca on chosen d=30
+    master_PCA_by_type(input_matrix_1000d, n_dim=30, pca_type='dro', plot=False, savefile=None) # dro on chosen d=30
